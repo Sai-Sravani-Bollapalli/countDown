46a570f74ba08e9bdb86b6481ea3f6b44a4bcc55
Sai Sravani Bollapalli <71527522+Sai-Sravani-Bollapalli@users.noreply.github.com>
2021-03-18 12:35:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+let countdown=document.querySelector(".count");
+countdown.addEventListener("click",function(){
+    countdown.innerText=10
+    setTimeout(function(){
+        countdown.innerText=9
+        setTimeout(function(){
+            countdown.innerText=8
+            setTimeout(function(){
+                countdown.innerText=7
+                setTimeout(function(){
+                    countdown.innerText=6
+                    setTimeout(function(){
+                        countdown.innerText=5
+                        setTimeout(function(){
+                            countdown.innerText=4
+                            setTimeout(function(){
+                                countdown.innerText=3
+                                setTimeout(function(){
+                                    countdown.innerText=2
+                                    setTimeout(function(){
+                                        countdown.innerText=1
+                                        setTimeout(function(){
+                                            countdown.innerText='Happy Independence Day';
+                                        },1000)
+                                    },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)
+})
